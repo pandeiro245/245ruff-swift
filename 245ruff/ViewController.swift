2972d5f01cf1e8e1c17b3e60597eb40ff0751e0e
Hironobu Nishikokura <nishiko@mindia.jp>
2014-09-03 15:25:05 +0900

    Initial Commit

---
+++ 245ruff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  245ruff
+//
+//  Created by nishiko on 2014/09/03.
+//  Copyright (c) 2014年 nishiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
